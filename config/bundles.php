837c2e9b9684c7a837995d50dd630d5bf3bf82e8
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-06 21:33:50 +0200

    :heavy_plus_sign: added knp paginator bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
