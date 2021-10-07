0f1f8ce899807e1ea4c32a3bd59cc2337e2577cf
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-07 08:04:28 +0200

    ✨ Get current page and article's comments

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Service\ArticleService;
+use App\Service\CommentService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,15 +15,18 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article/{category}/{slug}")
      */
-    public function show(string $category, string $slug, ArticleService $articleService): Response
+    public function show(string $category, string $slug, ArticleService $articleService, CommentService $commentService, Request $request): Response
     {
         $categories = $articleService->getCategories();
+        $page = $request->get("page", 1);
+        
         if (!in_array($category, $categories)) {
             throw new NotFoundHttpException();
         }
 
         return $this->render('article/show.html.twig', [
             'article' => $articleService->getArticle($category, $slug),
+            'comments' => $commentService->getArticleComments($category, $slug, $page)
         ]);
     }
 }
