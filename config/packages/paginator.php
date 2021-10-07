19399aa9f961de3597624b753677a50dfebaa459
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-07 08:00:24 +0200

    :wrench: Created config file for knp paginator bundle

---
+++ config/packages/paginator.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $configurator): void
+{
+    $configurator->extension('knp_paginator', [
+        'page_range' => 5,                        // number of links showed in the pagination menu (e.g: you have 10 pages, a page_range of 3, on the 5th page you'll see links
+        'default_options' => [
+            'page_name' => 'page',                // page query parameter name
+            'sort_field_name' => 'sort',          // sort field query parameter name
+            'sort_direction_name' => 'direction', // sort direction query parameter name
+            'distinct' => true,                   // ensure distinct results, useful when ORM queries are using GROUP BY statements
+            'filter_field_name' => 'filterField', // filter field query parameter name
+            'filter_value_name' => 'filterValue'  // filter value query parameter name
+        ],
+        'template' => [
+            'pagination' => '@KnpPaginator/Pagination/sliding.html.twig',     // sliding pagination controls template
+            'sortable' => '@KnpPaginator/Pagination/sortable_link.html.twig', // sort link template
+            'filtration' => '@KnpPaginator/Pagination/filtration.html.twig'   // filters template
+        ]
+    ]);
+};
