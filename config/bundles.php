62c629f741ebbd935e7c9d52c2940a68e759f11d
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-08 22:58:00 +0200

    :fire: Removed knp paginator from bundle registration file

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
